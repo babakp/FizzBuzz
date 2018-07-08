87b1b3130a13347d6f6d3459af9b4a08556295d0
babakp <babak.payvand@outlook.com>
2018-07-09 00:04:29 +1000

    Add files via upload

---
+++ GenerateFizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using FizzBuzzLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateFizzBuzz
+{
+    class Program
+    {
+        private static int i;
+
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            int printNumberFrom = 1;
+            int printNumberTo = 100;
+            for (i = printNumberFrom; i <= printNumberTo; i++)
+                Console.WriteLine(fizzBuzz.Output(i));
+        }
+    }
+}
